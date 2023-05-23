0f7deb4d4f00063c6bdb3c9a81e563394ac285fa
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-05-23 17:59:23 +0530

    Added StackNavigator from React Navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/// <reference types="nativewind/types" />
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+
+type RootStack = {
+  Login: undefined;
+  Register: undefined;
+}
+
+const Stack = createNativeStackNavigator<RootStack>();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+        headerStyle: {
+          backgroundColor: "#2C6BED",
+        },
+        headerTitleStyle: {
+          color: "white",
+        },
+        headerTintColor: "white",
+      }}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
