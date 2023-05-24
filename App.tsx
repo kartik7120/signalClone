a47acd89398a1c5a190f11b4dfea0d235e7e5a6f
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-05-24 18:12:58 +0530

    Added new Add chat screen and conditionally rendering the login screens

--- App.tsx
+++ App.tsx
@@ -1,17 +1,27 @@
 /// <reference types="nativewind/types" />
+import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import { auth } from './firebase';
+import HomeHeaderLeft from './components/HomeHeaderLeft';
+import HomeHeaderRight from './components/HomeHeaderRight';
+import AddChatScreen from './screens/AddChatScreen';
 
 type RootStack = {
   Login: undefined;
   Register: undefined;
+  Home: undefined;
+  AddChat: undefined;
 }
 
-const Stack = createNativeStackNavigator<RootStack>();
+const Stack = createStackNavigator<RootStack>();
 
 export default function App() {
+  const currentUser = auth.currentUser;
+
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{
@@ -23,8 +33,33 @@ export default function App() {
         },
         headerTintColor: "white",
       }}>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Home" options={{
+          title: "Signal",
+          headerStyle: {
+            backgroundColor: "#fff",
+          },
+          headerTitleStyle: {
+            color: "#000",
+
+          },
+          headerTitleAlign: "center",
+          headerTintColor: "#000",
+          headerLeft: HomeHeaderLeft,
+          headerRight: HomeHeaderRight
+
+        }} component={HomeScreen} />
+        {currentUser ? (
+          null
+        ) : (
+          <>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Register" component={RegisterScreen} />
+          </>
+        )}
+        <Stack.Screen name="AddChat" component={AddChatScreen} options={{
+          title: "Add a new chat",
+          headerBackTitle: "Chats",
+        }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
