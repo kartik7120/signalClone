3bb7566202b0934bbb3fc1dbd642fbcce7d7027f
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-05-25 22:38:33 +0530

    Added some dependencies for expo web

--- App.tsx
+++ App.tsx
@@ -9,12 +9,17 @@ import { auth } from './firebase';
 import HomeHeaderLeft from './components/HomeHeaderLeft';
 import HomeHeaderRight from './components/HomeHeaderRight';
 import AddChatScreen from './screens/AddChatScreen';
+import ChatScreen from './screens/ChatScreen';
 
-type RootStack = {
+export type RootStack = {
   Login: undefined;
   Register: undefined;
   Home: undefined;
   AddChat: undefined;
+  Chat: {
+    id: string;
+    chatName: string;
+  };
 }
 
 const Stack = createStackNavigator<RootStack>();
@@ -24,7 +29,7 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{
+      <Stack.Navigator initialRouteName='Login' screenOptions={{
         headerStyle: {
           backgroundColor: "#2C6BED",
         },
@@ -40,26 +45,19 @@ export default function App() {
           },
           headerTitleStyle: {
             color: "#000",
-
           },
           headerTitleAlign: "center",
           headerTintColor: "#000",
           headerLeft: HomeHeaderLeft,
           headerRight: HomeHeaderRight
-
         }} component={HomeScreen} />
-        {currentUser ? (
-          null
-        ) : (
-          <>
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Register" component={RegisterScreen} />
-          </>
-        )}
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="AddChat" component={AddChatScreen} options={{
           title: "Add a new chat",
           headerBackTitle: "Chats",
         }} />
+        <Stack.Screen name='Chat' component={ChatScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- components/CustomListItem.tsx
+++ components/CustomListItem.tsx
@@ -1,28 +1,49 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ListItem, Avatar } from '@rneui/themed';
+import { useNavigation } from '@react-navigation/native';
+import { collection, limit, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { db } from '../firebase';
 
 interface Props {
     id: string;
     chatName: string;
-    enterChat: any;
 }
 
-export default function CustomListItem() {
+export default function CustomListItem(props: Props) {
+    const navigation = useNavigation();
+    const [chatMessages, setChatMessages] = useState<any[]>([]);
+
+    useEffect(() => {
+        const q = query(collection(db, "chats", props.id, "messages"), orderBy("timestamp", "desc"), limit(1));
+        const unsubscribe = onSnapshot(collection(db, "chats", props.id, "messages"), (snapshot: any) => {
+            setChatMessages(snapshot.docs.map((doc: any) => doc.data()))
+        })
+        return unsubscribe;
+    }, [props.id])
+
+    const handlePress = () => {
+        navigation.navigate("Chat" as never, {
+            id: props.id,
+            chatName: props.chatName
+        } as never)
+    }
+
     return (
-        <ListItem>
+        <ListItem onPress={handlePress} bottomDivider>
             <Avatar
                 rounded
                 source={{
-                    uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+                    uri: chatMessages.length > 0 ? chatMessages[0].photoURL
+                        : 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
                 }}
             />
             <ListItem.Content>
                 <ListItem.Title style={{ fontWeight: "800" }}>
-                    Youtube Chat
+                    {props.chatName}
                 </ListItem.Title>
                 <ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail">
-                    This is a test subtitle
+                    {chatMessages.length > 0 ? chatMessages[0].displayName + ": " + chatMessages[0].message : ""}
                 </ListItem.Subtitle>
             </ListItem.Content>
         </ListItem>
